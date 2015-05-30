6485565883584b51538039843d956e1c82ea03d7
carm <carmelo.piccione@gmail.com>
2015-05-30 00:38:56 -0400

    working aspcud install script

--- bin/install_aspcud.sh
+++ bin/install_aspcud.sh
@@ -1,89 +1,143 @@
-#!/usr/bin/env ocamlscript
-let open Ocamlscript.Std in
-begin
-Ocaml.packs :=
-  ["extlib";"re";"unix";"cmdliner";"fileutils";"re.posix";"containers";"shell_support"]
-end
---
-()
-open Cmdliner 
-open Shell_support
+#!/usr/bin/env bash
 
-let target_default = Shell.opam_bin_root
+arg0=$1
+get_target() {
+  local target_default=$HOME/local
 
-let target =
-  let doc = "Specifies the target installation directory." in
-  Arg.(value & opt string target_default & info ["o";"target"] ~doc
-         ~docv:"DIR")
-
-let defaut_url =
-  match Shell.os_type with 
-  | `Darwin -> 
-    "http://sourceforge.net/projects/potassco/files/aspcud/1.9.1/aspcud-1.9.1-macos-10.9.tar.gz"
-  | `MingW64 -> "http://sourceforge.net/projects/potassco/files/aspcud/1.9.1/aspcud-1.9.1-win64.zip"
-  | `Linux (* Default to linux *)
-  | _ -> "http://sourceforge.net/projects/potassco/files/aspcud/1.9.1/aspcud-1.9.1-x86_64-linux.tar.gz"
-
-let url =
-  let doc = "Url to fetch aspcud binaries from." in
-  Arg.(value & opt string defaut_url & info ["u";"url"] ~doc
-         ~docv:"URL")
-
-let print s = Printf.printf "[install_aspcud.ml]: %s\n" s
-
-let fetch_package url = 
-  let open Shell.Infix in
-  let basename = FilePath.basename url in begin
-  match String.lowercase @@ FilePath.get_extension basename with
-    | "tgz" -> `Ok (FilePath.replace_extension basename "tar.gz")
-    | "zip" | "gz" | "bzip2" -> `Ok basename
-    | s -> `Error (false, "unknown package extension: " ^ s)
-  end >>= fun output_file ->
-  Wget.run
-    ~no_check_certificate:true
-    ~output_documents:output_file
-    url >>| fun res -> print res;output_file
-
-let decompress filename =
-  let open Shell.Infix in
-  let chopped = FilePath.chop_extension filename in
-  FileUtil.rm ~recurse:true ~force:FileUtil.Force [chopped; FilePath.chop_extension chopped];
-  Decompress.run filename
-
-let extract_tar_maybe filename =
-  let open Shell.Infix in
-  match FilePath.get_extension filename with 
-  | "tar" -> Shell.run @@ "tar xvf " ^ filename >>= fun res -> ignore(res);
-    `Ok (FilePath.chop_extension filename)
-  | _ -> `Ok filename
+  target=${arg0}
+  if [ -z "${target}" ]; then
+    target=${target_default}
+  fi
+  target_help="Specifies the target installation directory. Defaults to ${target_default}"
+}
 
 
-let read_all (dir:string) = 
-  let dir = Unix.opendir dir in
-  let rec iter l = try 
-      let entry = Unix.readdir dir in 
-      if String.contains_from entry 0 '.' then iter l else
-      entry::(iter l) with End_of_file -> l in
-  iter []
+get_url() {
 
-let install ~target unpacked_dir =
-  let files = List.map (FilePath.concat unpacked_dir)
-     (read_all unpacked_dir) in
-  let bin = FilePath.concat target "bin" in
-  Shell.cp ~recurse:true files bin; `Ok ("installed to " ^ bin)
+  local os_type=$OSTYPE
+  local mach_type=$MACHTYPE
+ 
+  # Defaults to 64 bit linux 
+  url="http://sourceforge.net/projects/potassco/files/aspcud/1.9.1/aspcud-1.9.1-x86_64-linux.tar.gz"
 
-let run target url =
-  let open Shell.Infix in
-  print @@ "about to fetch: " ^ url;
-  fetch_package url >>= fun s -> print @@ "about to decompress: " ^ s;
-  decompress s >>= fun s -> print @@ "about to extract: " ^ s;
-  extract_tar_maybe s >>= fun s ->
-  print @@ "about to install " ^ s ^ " to " ^ target;
-  install ~target s
+  if [[ $os_type == *"linux"* ]]; then
+    if [[ $mach_type == *"64"* ]]; then
+      echo "64 bit Linux detected."
+      url="http://sourceforge.net/projects/potassco/files/aspcud/1.9.1/aspcud-1.9.1-x86_64-linux.tar.gz"
+    else
+      echo "32 bit Linux detected."
+      url="http://sourceforge.net/projects/potassco/files/aspcud/1.9.0/aspcud-1.9.0-x86-linux.tar.gz"
+    fi
+  elif [[ $os_type == *"darwin"* ]]; then
+    echo "MacOS detected."
+    url="http://sourceforge.net/projects/potassco/files/aspcud/1.9.1/aspcud-1.9.1-macos-10.9.tar.gz"
+  elif [[ $os_type == *"win"* ]]; then
+    url="http://sourceforge.net/projects/potassco/files/aspcud/1.9.1/aspcud-1.9.1-win64.zip"
+  fi
+}
+
+
+fetch_package() {
+
+  local base=$(basename ${url})
+  base="${base%.*}"
+  filename="${base%.*}"
+
+  compressed_pkg=${filename}.tar.gz
+
+ wget --no-check-certificate --output-document=${compressed_pkg} ${url}
+
+}
+
+
+decompress(){
+  gzip -f -d ${compressed_pkg}
+
+  tar xvf ${filename}.tar
   
-let cmd =
-  let doc = "Compile and install aspcud." in
-  Term.(ret (pure run $ target $ url)),
-  Term.info "install_aspcud.ml" ~version:"1.0" ~doc 
+}
 
-let () = print "start"; match Term.eval cmd with `Error _ -> exit 1 | _ -> exit 0
+preinstall_clean() {
+  rm -rf ${filename}/examples
+}
+
+install() {
+ 
+  if [ ! -d "${target}/bin" ]; then
+    mkdir ${target}/bin
+  fi
+
+  echo Copying binaries to ${target}
+  cp -f ${filename}/* ${target}/bin/
+
+}
+
+setup_env() {
+
+  local solver_var="OPAMEXTERNALSOLVER"
+  local solver_path=${target}/bin
+  local solver_bin=${solver_path}/aspcud
+  local solver_string_cmt="# Set explicit external solver location for opam installations"
+  local solver_string="export ${solver_var}=${solver_bin}"
+
+  export PATH=$PATH:${solver_path}
+
+  local path_string_cmt="# Add location of aspcud solver to path"
+  local path_string="export PATH=\$PATH:${solver_path}"
+  profile=""
+  if [ -e "$HOME/.bash_profile" ]; then
+     profile=$HOME/.bash_profile 
+  elif [ -e "$HOME/.profile" ]; then
+     profile=$HOME/.profile
+  elif [ -e "$HOME/.bashrc" ]; then
+     profile=$HOME/.bashrc   
+  else
+      echo "WARNING: No profile script found in your home directory to update!"
+      echo "Please manually add the following to your shell start script: "
+      echo "---------------------------------------------------------------"
+      echo ${path_string}
+      echo ${solver_string}
+  fi
+
+  solver_var_txt=`grep -o ${solver_var} ${profile}`
+
+  if [ -z "${solver_var_txt}" ]; then
+    echo "Adding \"${solver_var}\" to ${profile}" 
+    echo ${solver_string_cmt} >> ${profile}
+    echo ${solver_string} >> ${profile}
+  fi
+
+  path_txt=`grep -o \"${solver_path}\" ${profile}`
+
+  if [ -z "${path_txt}" ]; then
+    echo "Adding \"${solver_path}\" to path in ${profile}" 
+    echo ${path_string_cmt} >> ${profile}
+    echo ${path_string} >> ${profile}
+  fi
+
+
+}
+
+check_if_installed() {
+
+res=`which aspcud`
+
+if [ ! -z "${res}" ]; then
+  echo aspcud already installed at \"${res}\", aborting.
+  exit 1
+fi
+
+}
+
+run() {
+  get_target
+  get_url
+  fetch_package
+  decompress
+  preinstall_clean
+  install
+  setup_env
+}
+
+check_if_installed
+run
