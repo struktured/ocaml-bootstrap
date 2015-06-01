69791ff52adbabc7a313d7287381dfdcb58374b1
carm <carmelo.piccione@gmail.com>
2015-06-01 18:45:53 -0400

    fix /bin/sh typo

--- bin/ocaml-profiles-bootstrap.sh
+++ bin/ocaml-profiles-bootstrap.sh
@@ -1,4 +1,4 @@
-#!bin/sh
+#!/bin/sh
 
 opam pin add -y -k git shell-support https://github.com/struktured/ocaml-shell-support#stable
 opam pin add -y -k git ocaml-profiles https://github.com/struktured/ocaml-profiles#stable
